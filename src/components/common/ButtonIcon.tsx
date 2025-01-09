ee328d72d2cd2d6e3343ed30c90e219674f5f643
Jose Marcel <lopezjosemarcel11@gmail.com>
2025-01-09 11:58:33 -0300

    update 6.1
    update 6.1

--- src/components/common/ButtonIcon.tsx
+++ src/components/common/ButtonIcon.tsx
@@ -10,7 +10,7 @@ export default function ButtonIcon({ params }: IconButtonProps) {
     <>
       <button
         type={params.type}
-        className="bg-orange-500 text-white px-3 py-1 text-sm rounded-lg hover:bg-orange-600 mt-2 flex justify-center items-center gap-2 
+        className="bg-orange-500 text-white px-3 py-1 text-xs rounded-lg hover:bg-orange-600 mt-2 flex justify-center items-center gap-2 
   sm:px-4 sm:py-2 sm:text-base"
       >
         {params.label}
